5260b5de74ae9ef49f50ca80e5dd94ec062bad2e
ONeil <evgenprogrammer@gmail.com>
2025-06-25 20:02:40 +0300

    init project

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/EvgenyiK/phoneBook
+
+go 1.24.4
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
+
+replace github.com/EvgenyiK/phoneBook => ./
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	phonebook "github.com/EvgenyiK/phoneBook"
+)
+
+var MIN = 0
+var MAX = 26
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func getString(length int64) string {
+	startChar := "A"
+	temp := ""
+	var i int64 = 1
+	for {
+		myRand := random(MIN, MAX)
+		newChar := string(startChar[0] + byte(myRand))
+		temp = temp + newChar
+		if i == length {
+			break
+		}
+		i++
+	}
+	return temp
+}
+
+func main() {
+	data, err := phonebook.ListUsers()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range data {
+		fmt.Println(v)
+	}
+
+	SEED := time.Now().Unix()
+	rand.Seed(SEED)
+	random_username := getString(5)
+
+	t := phonebook.Userdata{
+		Username:    random_username,
+		Name:        "Evgen",
+		Surname:     "Kartapov",
+		Description: "This is me!"}
+
+	id := phonebook.AddUser(t)
+	if id == -1 {
+		fmt.Println("There was an error adding user", t.Username)
+	}
+
+	err = phonebook.DeleteUser(id)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Попытка повторного удаления!
+	err = phonebook.DeleteUser(id)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	id = phonebook.AddUser(t)
+	if id == -1 {
+		fmt.Println("There was an error adding user", t.Username)
+	}
+
+	t = phonebook.Userdata{
+		Username:    random_username,
+		Name:        "Evgen",
+		Surname:     "Kartapov",
+		Description: "Fuck you =)"}
+
+	err = phonebook.UpdateUser(t)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
